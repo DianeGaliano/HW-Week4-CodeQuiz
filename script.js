386bdd2fcdc688f9dcfe87b578a668e94a969a55
Diane Galiano <dianegaliano09@gmail.com>
2021-08-21 13:40:49 -0500

    added a function for correct and wrong answer score that will be stored on the clients storage. Also added answer, done, score, and start button var's

---
+++ script.js
@@ -0,0 +1,4 @@
+var anserChoice = document.querySelector(".answer-choice");
+var done = document.querySelector(".done");
+var score = document.querySelector(".high-score");
+var startButton = document.querySelector(".start-button");
